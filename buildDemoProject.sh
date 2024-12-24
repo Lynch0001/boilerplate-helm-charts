d73be782f296a8099ca2ac36f411e0c437febebb
timothylynch <lynch0001@gmail.com>
2024-12-24 06:53:05 -0500

    update

--- buildDemoProject.sh
+++ buildDemoProject.sh
@@ -10,7 +10,7 @@ echo "started helm build"
 TARGET_PROJECT=$1
 BOILERPLATE_PATH="/Users/timothylynch/workspace/app"
 BOILERPLATE_BINARY="boilerplate_linux_amd64"
-REPO_PATH="/Users/timothylynch/workspace/app/boilerplate-helm-charts"
+REPO_PATH="boilerplate-helm-charts"
 
 sed -i 's/Project: .*/Project: '"$TARGET_PROJECT"'/g' boilerplate-helm-charts/build_vars.yml
 
